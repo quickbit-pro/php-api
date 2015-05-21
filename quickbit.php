5d44615a7f59f15de27043ca7b40474fc4a3ca19
KristijanBenkovic <benkovic.kristijan@gmail.com>
2015-05-21 14:40:43 +0200

    added crypto curreny

--- quickbit.php
+++ quickbit.php
@@ -1,6 +1,6 @@
 <?php
 class quickbit {
-	private $api_key, $api_secret, $amount, $currency, $nounce, $customer_info, $invoice_id, $response, $post_fields, $order_hash,$microtime;
+	private $api_key, $api_secret, $amount, $crypto_currency, $currency, $nounce, $customer_info, $invoice_id, $response, $post_fields, $order_hash,$microtime;
 	public function getResponse() {
 		return $this->response;
 	}
@@ -10,10 +10,14 @@ class quickbit {
 	public function setInvoice_id($invoice_id) {
 		$this->invoice_id = $invoice_id;
 	}
+
+	public function setCrypto_currency($crypto_currency) {
+		$this->crypto_currency = $crypto_currency;
+	}
 	function __construct() {
 	}
 	function connect() {
-		$ch = curl_init ( "https://quickbit.pro/api_handler/" );
+		$ch = curl_init ( "http://quickbit.localhost/api_handler/" );
 		curl_setopt ( $ch, CURLOPT_POST, true );
 		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
 		curl_setopt ( $ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
@@ -22,7 +26,6 @@ class quickbit {
 		curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, 2 );
 		curl_setopt ( $ch, CURLOPT_POSTFIELDS, $this->post_fields );
 		$responseData = curl_exec ( $ch );
-		
 		$this->response = json_decode ( $responseData, true );
 	}
 	function open_invoice($api_key, $api_secret, $amount, $currency) {
@@ -32,7 +35,7 @@ class quickbit {
 		$this->currency = $currency;
 		$this->microtime=microtime(true);
 		$this->nounce ();
-		
+
 		$this->post_fields = array (
 				'amount' => utf8_encode ( $this->amount ),
 				'currency' => utf8_encode ( $this->currency ),
@@ -41,6 +44,7 @@ class quickbit {
 				'customer_info' => $this->customer_info,
 				'invoice_id' => utf8_encode ( $this->invoice_id ),
 				'microtime'=>$this->microtime,
+				'crypto_currency' => $this->crypto_currency, 
 				'type' => 'create_invoice' 
 		);
 		$this->connect ();
@@ -65,4 +69,3 @@ class quickbit {
 		$this->nounce = hash ( 'sha512', $this->api_secret . $this->microtime . $this->api_key );
 	}
 }
-?>
