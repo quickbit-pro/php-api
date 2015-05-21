f259e4da7cd666e4907943c4e87590891921d176
KristijanBenkovic <benkovic.kristijan@gmail.com>
2015-05-21 14:42:03 +0200

    crypto currency added

--- quickbit_sandbox.php
+++ quickbit_sandbox.php
@@ -1,6 +1,6 @@
 <?php
-class quickbit_sandbox {
-	private $api_key, $api_secret, $amount, $currency, $nounce, $customer_info, $invoice_id, $response, $post_fields, $order_hash,$microtime;
+class quickbit {
+	private $api_key, $api_secret, $amount, $crypto_currency, $currency, $nounce, $customer_info, $invoice_id, $response, $post_fields, $order_hash,$microtime;
 	public function getResponse() {
 		return $this->response;
 	}
@@ -9,6 +9,10 @@ class quickbit_sandbox {
 	}
 	public function setInvoice_id($invoice_id) {
 		$this->invoice_id = $invoice_id;
+	}
+
+	public function setCrypto_currency($crypto_currency) {
+		$this->crypto_currency = $crypto_currency;
 	}
 	function __construct() {
 	}
@@ -22,7 +26,6 @@ class quickbit_sandbox {
 		curl_setopt ( $ch, CURLOPT_SSL_VERIFYHOST, 2 );
 		curl_setopt ( $ch, CURLOPT_POSTFIELDS, $this->post_fields );
 		$responseData = curl_exec ( $ch );
-		
 		$this->response = json_decode ( $responseData, true );
 	}
 	function open_invoice($api_key, $api_secret, $amount, $currency) {
@@ -32,7 +35,7 @@ class quickbit_sandbox {
 		$this->currency = $currency;
 		$this->microtime=microtime(true);
 		$this->nounce ();
-		
+
 		$this->post_fields = array (
 				'amount' => utf8_encode ( $this->amount ),
 				'currency' => utf8_encode ( $this->currency ),
@@ -41,6 +44,7 @@ class quickbit_sandbox {
 				'customer_info' => $this->customer_info,
 				'invoice_id' => utf8_encode ( $this->invoice_id ),
 				'microtime'=>$this->microtime,
+				'crypto_currency' => $this->crypto_currency, 
 				'type' => 'create_invoice' 
 		);
 		$this->connect ();
@@ -65,4 +69,3 @@ class quickbit_sandbox {
 		$this->nounce = hash ( 'sha512', $this->api_secret . $this->microtime . $this->api_key );
 	}
 }
-?>
